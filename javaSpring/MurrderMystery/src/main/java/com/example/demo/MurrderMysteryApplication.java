d208e6ba2ca482cf54245f14af977e767d51fad7
ccartwright123 <ccartwright@qa.com>
2021-07-27 10:40:07 +0100

    MUR-28 #comment suspect object

---
+++ javaSpring/MurrderMystery/src/main/java/com/example/demo/MurrderMysteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurrderMysteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurrderMysteryApplication.class, args);
+	}
+
+}
